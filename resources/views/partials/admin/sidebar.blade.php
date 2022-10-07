10f62e25930d41e9630fe20e59ee6b41efd95c93
tom oliver chua <chuatomoliver@gmail.com>
2022-10-07 10:14:14 +0800

    Adding pos tab in admin

--- resources/views/partials/admin/sidebar.blade.php
+++ resources/views/partials/admin/sidebar.blade.php
@@ -70,6 +70,14 @@
           <span class="nav-link-text ms-1 text-uppercase">Accounts</span>
         </a>
       </li>
+      <li class="nav-item">
+        <a class="nav-link text-white" href="{{ route("customer.products") }}">
+          <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
+            <i class="fas fa-shopping-cart" style="font-size: 17px"></i>
+          </div>
+          <span class="nav-link-text ms-1 text-uppercase">POS</span>
+        </a>
+      </li>
   
    
     </ul>
